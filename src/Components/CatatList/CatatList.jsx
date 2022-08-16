39c0e054d4f14d2e2b339acbf1265c6782a89ff3
kreindo <ahmadsan4p@gmail.com>
2022-08-16 14:43:14 +0800

    add 'tdk ada catatan' when there's no note present

--- src/Components/CatatList/CatatList.jsx
+++ src/Components/CatatList/CatatList.jsx
@@ -1,20 +1,25 @@
-import React from 'react';import { showFormattedDate } from '../../utils';
+import React from 'react';
+import { showFormattedDate } from '../../utils';
 import CatatItem from '../CatatItem/CatatItem';
 
 const ContactList = (props) => {
   const { catatans, onDelete } = props;
   return (
     <div className="notes-list">
-      {catatans.map((catatan) => (
-        <CatatItem
-          key={catatan.id}
-          id={catatan.id}
-          onDelete={onDelete}
-          // onArchive={onArchive}
-          timestamp={showFormattedDate(catatan.createdAt)}
-          {...catatan}
-        />
-      ))}
+      {catatans.length > 0 ? (
+        catatans.map((catatan) => (
+          <CatatItem
+            key={catatan.id}
+            id={catatan.id}
+            onDelete={onDelete}
+            // onArchive={onArchive}
+            timestamp={showFormattedDate(catatan.createdAt)}
+            {...catatan}
+          />
+        ))
+      ) : (
+        <p className="notes-list__empty-message">Tidak ada catatan</p>
+      )}
     </div>
   );
 };
